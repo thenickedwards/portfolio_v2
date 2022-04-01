8350ae164b7b64935c7f9551b8e920a3b9326751
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-03-31 22:15:23 -0700

    Wrote my name on it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <>
+      <Nav />
+    
     </>
   );
 }
 
 export default App;
+// Check out icon avatars for footer
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <section>
+        <h1>Nick Edwards</h1>
+    </section>
+  )
+}

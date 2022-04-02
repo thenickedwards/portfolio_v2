97c5df1db2e20de69018fd0eed37945a095306cd
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-04-01 20:35:03 -0700

    footer icons added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import emailIcon from '../images/icon-email-fast.png'
 import githubIcon from '../images/icon-github.png'
+import linkedinIcon from '../images/icon-linkedin.png'
+import resumeIcon from '../images/icon-file-document.png'
 
 export default function Footer() {
   return (
-    <section>
-        <div>
+    <section className='footer-container'>
+        <div className='footer-icons'>
+            <a><img src={emailIcon}></img></a>
             <a><img src={githubIcon}></img></a>
+            <a><img src={linkedinIcon}></img></a>
+            <a><img src={resumeIcon}></img></a>
         </div>
     </section>
   )

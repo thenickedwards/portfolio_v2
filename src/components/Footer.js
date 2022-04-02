829e972926c332d14a5b23e87c5306d32492c776
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-04-01 21:08:27 -0700

    Built out footer with dark icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import emailIcon from '../images/icon-email-fast.png'
-import githubIcon from '../images/icon-github.png'
-import linkedinIcon from '../images/icon-linkedin.png'
-import resumeIcon from '../images/icon-file-document.png'
+import emailIconGray from '../images/icon-email-fast-gray.png'
+import githubIconGray from '../images/icon-github-gray.png'
+import linkedinIconGray from '../images/icon-linkedin-gray.png'
+import resumeIconGray from '../images/icon-file-document-gray.png'
 
 export default function Footer() {
   return (
     <section className='footer-container'>
         <div className='footer-icons'>
-            <a><img src={emailIcon}></img></a>
-            <a><img src={githubIcon}></img></a>
-            <a><img src={linkedinIcon}></img></a>
-            <a><img src={resumeIcon}></img></a>
+            <a><img src={emailIconGray}></img></a>
+            <a><img src={githubIconGray}></img></a>
+            <a><img src={linkedinIconGray}></img></a>
+            <a><img src={resumeIconGray}></img></a>
         </div>
     </section>
   )

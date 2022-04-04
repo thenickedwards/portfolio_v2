9257572c639fe10a7e4ed17e89cab0c9fa529b94
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-04-03 22:02:00 -0700

    Added fine touches to form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,18 +4,22 @@ import { Form, Button } from "react-bootstrap/";
 export default function Contact() {
   return (
     <section className="contact-container">
-      <Form className="col-11 py-3 px-5 mt-4 contact-form">
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form className="col-10 py-3 px-5 mt-4 contact-form">
+        <Form.Group className="mb-3" controlId="Form.ControlInput-Name">
             <p>Please note: This form is not yet functional! (The backend is still under construction.) Please use the email link in the footer to send me a message. Thanks! <br />- N</p>
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Provide me your email so I can reply!" />
+          <Form.Label>Name</Form.Label>
+          <Form.Control required type="text" placeholder="Who's writing?" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="mb-3" controlId="Form.ControlInput-Email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control required type="email" placeholder="Provide me your email so I can reply!" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Form.ControlInput-Message">
           <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Compose your message here. Looking forward to hearing from you!"/>
+          <Form.Control required as="textarea" rows={3} placeholder="Compose your message here. Looking forward to hearing from you!"/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="I'm not a robot" />
+          <Form.Check required type="checkbox" label="I'm not a robot" />
         </Form.Group>
         <Button variant="dark" type="submit">
           Submit

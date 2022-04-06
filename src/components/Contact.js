a7787f6c7cb5b55d26802c3ef4ec4a630a751929
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-04-06 13:42:46 -0700

    Updated form placeholders

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,15 +8,15 @@ export default function Contact() {
         target="_blank" action="https://formsubmit.co/nickedwards.dev@gmail.com" method="POST">
         <Form.Group className="mb-3" controlId="Form.ControlInput-Name">
           <Form.Label>Name</Form.Label>
-          <Form.Control required type="text" name="name" placeholder="Who's writing?" />
+          <Form.Control required type="text" name="name" placeholder="Let me know who you are!" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="Form.ControlInput-Email">
           <Form.Label>Email</Form.Label>
-          <Form.Control required type="email" name="email" placeholder="Provide me your email so I can reply!" />
+          <Form.Control required type="email" name="email" placeholder="And shoot me your email so I can reply!" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="Form.ControlInput-Message">
           <Form.Label>Message</Form.Label>
-          <Form.Control required as="textarea" name="message" rows={3} placeholder="Compose your message here. Looking forward to hearing from you!"/>
+          <Form.Control required as="textarea" name="message" rows={3} placeholder="You can compose your message here or use the email link in the footer. Looking forward to hearing from you!"/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check required type="checkbox" label="I'm not a robot" />

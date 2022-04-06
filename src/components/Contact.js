329d578620b652f292509a497cd886abf45850bb
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-04-06 13:20:55 -0700

    Added funciontality to form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,19 @@ import { Form, Button } from "react-bootstrap/";
 export default function Contact() {
   return (
     <section className="contact-container">
-      <Form className="col-10 py-3 px-5 mt-4 contact-form">
+      <Form className="col-10 py-3 px-5 mt-4 contact-form"
+        target="_blank" action="https://formsubmit.co/nickedwards.dev@gmail.com" method="POST">
         <Form.Group className="mb-3" controlId="Form.ControlInput-Name">
-            <p>Please note: This form is not yet functional! (The backend is still under construction.) Please use the email link in the footer to send me a message. Thanks! <br />- N</p>
           <Form.Label>Name</Form.Label>
-          <Form.Control required type="text" placeholder="Who's writing?" />
+          <Form.Control required type="text" name="name" placeholder="Who's writing?" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="Form.ControlInput-Email">
           <Form.Label>Email</Form.Label>
-          <Form.Control required type="email" placeholder="Provide me your email so I can reply!" />
+          <Form.Control required type="email" name="email" placeholder="Provide me your email so I can reply!" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="Form.ControlInput-Message">
           <Form.Label>Message</Form.Label>
-          <Form.Control required as="textarea" rows={3} placeholder="Compose your message here. Looking forward to hearing from you!"/>
+          <Form.Control required as="textarea" name="message" rows={3} placeholder="Compose your message here. Looking forward to hearing from you!"/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check required type="checkbox" label="I'm not a robot" />
